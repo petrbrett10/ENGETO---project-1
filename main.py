dffe0b26a2d5d4c285de29c6fdf0d1752dd2c584
petrbrett10 <petrbrettschneider@gmail.com>
2025-04-13 09:33:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 # hlavička
 
 """
@@ -8,8 +6,7 @@ projekt_1.py: první projekt do Engeto Online Python Akademie
 author: Petr Brettschneider
 email: petrbrettschneiderr@gmail.com
 """
-# databáze uživatelů
-
+# users database
 databaze_uzivatelu ={
     "bob":"123" , 
     "ann":"pass123" , 
@@ -17,31 +14,23 @@ databaze_uzivatelu ={
     "liz":"pass123"
     }
 
-# object - počítání velkých
-# object - počítání malých
-# object - počítání čísel (int)
-# zadání jména
-jmeno_vstup = input ("username:")
+# username and password input
+username_in = input ("username: ")
+password_in = input ("password: ")
+print ("-" * 34)
 
-# zadání hesla
-heslo_vstup = input ("password:")
-print ("-----------------------------")
-# ověření uživatele a hesla
-if jmeno_vstup in databaze_uzivatelu:
-    if heslo_vstup == (databaze_uzivatelu.get (jmeno_vstup)):
-        print ("Welcome to the app,", jmeno_vstup)
+# users nad passwords control
+if username_in in databaze_uzivatelu:
+    if password_in == (databaze_uzivatelu.get (username_in)):
+        print ("Welcome to the app,", username_in, ".")
     else: 
-        print ("Wrong password. Program terminated.")
+        print ("Wrong password. Terminating the program.")
         exit()
 else:
-    print ("Unregistred user. Terminating the program")
+    print ("Unregistred user. Terminating the program.")
     exit()
 
-#print ("pokračuji")
-     
-
-# vstupní texty
-
+# texts for analyse
 texts = [
     '''Situated about 10 miles west of Kemmerer,     
     Fossil Butte is a ruggedly impressive
@@ -69,184 +58,97 @@ texts = [
     in modern oceans. Other fish such as paddlefish,
     garpike and stingray are also present.'''
 ]
-pocet_textu = int(len(texts))
+texts_amount = int(len(texts))
 
-print ("We have ", pocet_textu , " texts to be analyzed.")
-print ("-----------------------------")
-# výběr textu
-volba_textu = int(input("Enter a number btw. 1 and 3 to select:"))
-print ("-----------------------------")
+print ("We have ", texts_amount , " texts to be analyzed.")
+print ("-" * 34)
 
-# vyhodnocení vložení výběru textu
-if volba_textu<1 or volba_textu>pocet_textu:
-    print ("Špatná volba. Ukončuji program.")
+# text choice
+text_choice = input(f"Enter a number btw. 1 and {texts_amount} to select: ")
+print ("-" * 34)
+
+# ValueError check
+try:
+    int (text_choice)
+except ValueError:
+    print ("Wrong choice. Program terminated.")
+    exit()
+text_choice = int(text_choice)
+
+# text select check
+if text_choice < 1 or text_choice > texts_amount:
+    print ("Wrong choice. Program terminated.")
     exit()
 
-volba_textu = volba_textu - 1
-vybrany_text = texts[volba_textu]
-#print (vybrany_text)
-#print (type (vybrany_text))
+text_choice -= 1
+choosen_text = texts[text_choice]
 
-# definice setu - jednotlivá slova z řetězce
-text_k_analyze = list()
-# procházení textu - projde text písmeno po písmenu a najde konce slov - když narazí na mezeru, čárku nebo tečku, uloží slovo do listu "text_k_analyze"
-slovo=""
-for pismeno in vybrany_text:
-    if pismeno == " ":
-        #print ("mezera")
-        text_k_analyze.append (slovo)
-        slovo=""
-    elif pismeno == ".":
-        #print ("tečka")
-        text_k_analyze.append (slovo)
-        slovo=""
-    elif pismeno == ",":
-        #print ("čárka")
-        text_k_analyze.append (slovo)
-        slovo=""
-    elif pismeno == '\n':
-        #print ("Enter")
-        text_k_analyze.append (slovo)
-        slovo=""
-    else:
-        slovo=slovo+pismeno
-        #print (slovo)
-if len(slovo)>0:
-    text_k_analyze.append(slovo)    
-#print (type(text_k_analyze))
-#print (text_k_analyze)
-#print (len(text_k_analyze[9]))
-# odmazání prázdných řetězců z listu
-# posouzení slov v textu - pozice s prázdným textem
-pocet_slov_v_textu = 0
-prazdne_pozice = []
-for i in text_k_analyze:
-    if len(i)>0:
-        pocet_slov_v_textu = pocet_slov_v_textu + 1
-pozice_slova=1
-for word in text_k_analyze:
-    if len(word)<1:
-        #print ("nalezeno", pozice_slova)
-        prazdne_pozice.append (pozice_slova)
-        pozice_slova +=1
-    else:  
-        #print (word, len(word), pozice_slova)
-        pozice_slova +=1
-#for word in text_k_analyze:
-    #print (word,len(word))
-# vymazání prázdných pozic
-#print (prazdne_pozice)
-prazdne_pozice.sort (reverse=True)
-#print (prazdne_pozice)
-#print (text_k_analyze)
-for pozice in prazdne_pozice:
-    del text_k_analyze[pozice-1]
-    
-# kontrola listu bez "prázdných" slov    
-#print (text_k_analyze)
+# ".", "," deleting from selected text
+choosen_text = choosen_text.replace(".","")
+choosen_text = choosen_text.replace(",","")
 
-print ("There are ",pocet_slov_v_textu, "words in the selected text.")
+# text splut
+text_to_analyze = list()
+text_to_analyze = choosen_text.split()
 
-# počet slov začínajích velkým, malým písmenem a číslem
-zac_velkym = []
-zac_malym = []
-zac_cislem = []
-cele_velke =[]
-cele_male = []
-cele_cislo = []
-for word in text_k_analyze:
-    prvni_pismeno=str(word[0])
-    #print (word,prvni_pismeno)
-    if ord(prvni_pismeno) in range (65,90):
-        zac_velkym.append (word)
-    elif ord(prvni_pismeno) in range (97,122):
-        zac_malym.append (word)
-    elif ord(prvni_pismeno) in range (48,57):
-        zac_cislem.append (word)
+# words counting
+print ("There are ", len(text_to_analyze), "words in the selected text.")
 
-#print (zac_velkym)
-#print ("Slov začínajích velkým písmenem je:", len (zac_velkym))
+# words sorting
+titlecase_words_all = []
+uppercase_words =[]
+lowercase_words = []
+numeric_strings = []
+for word in text_to_analyze:
+    first_letter = str(word[0])
+    if ord(first_letter) in range (65,90):
+        titlecase_words_all.append (word)
 
-#print (zac_malym)
-#print ("Slov začínajích malým písmenem je:", len (zac_malym))
+for word in text_to_analyze:
+    numeric_strings = [word for word in text_to_analyze if word.isdigit()]
+    lowercase_words = [word for word in text_to_analyze if word.islower()]
+    uppercase_words = [word for word in text_to_analyze if word.isupper()]  
 
-#print (zac_cislem)        
-#print ("Čísel je:", len (zac_cislem))
+# Titlecase words counting
+only_title = len(titlecase_words_all) - len(uppercase_words)
 
-# kontrola slov psaných jen velkými písmeny
-for word in text_k_analyze:
-    cele_velke_status = 0
-    cislo_status = 0
-    cele_male_status = 0
-    for letter in word:
-        #print (word, letter, ord(letter))
-        if ord(letter) in range (65,90):
-            cele_velke_status = 1
-        if ord(letter) in range (97,122):
-            cele_male_status = 1
-        if ord(letter) in range (48,57):
-            cislo_status = 1      
-        
-    if cele_velke_status == 1 and cele_male_status == 0 and cislo_status ==0:
-        #print (cele_velke_status, word)
-        cele_velke.append(word)
-        #print (cele_velke_status)
-    if cele_velke_status == 0 and cele_male_status == 1 and cislo_status ==0:
-        #print (cele_male_status, word)
-        cele_male.append(word)
-        #print (cele_male_status)
-    if cele_velke_status == 0 and cele_male_status == 0 and cislo_status ==1:
-        #print (cislo_status, word)
-        cele_cislo.append(word)
-        #print (cislo_status)
-set_PV = set(zac_velkym)
-#print ("PV:",set_PV)
-set_CV = set(cele_velke)
-print (set_CV)
+# statistic output
+print ("There are ", only_title , "titlecase words.")
+print ("There are ", len(uppercase_words) , "uppercase words.")
+print ("There are ", len(lowercase_words) , "lowercase words.")
+print ("There are ", len(numeric_strings) , "numeric strings.")
 
-#rozdil_PV_CV = set_PV - set_CV
-#print ("Rozdil: ",rozdil_PV_CV)
-zac_velkym_rozdil = len(zac_velkym) - len(cele_velke)     
-print ("There are ",zac_velkym_rozdil, "titlecase words.")
-#print (zac_velkym)
-print ("There are ",len(cele_velke), "uppercase words.")
-#print (cele_velke)
-print ("There are ",len(cele_male), "lowercase words.")
-#print (cele_male)
-print ("There are ",len(cele_cislo), "numeric strings.")
-#print (cele_cislo)
-# součet čísel
-soucet_cisel = 0
-for cislo in cele_cislo:
-    cislo_int = int(cislo)
-    soucet_cisel+=cislo_int
-print ("The sum of all numbers is: ",soucet_cisel)
-#pocet_vyskytu_delek_slov
-max_delka=0
-for polozka in text_k_analyze:
-    delka_slova=len(polozka)
-    if delka_slova>max_delka:
-        max_delka = delka_slova #pocet znaku nejdelsiho slova
-    #print (delka_slova, max_delka)
-pocet_vyskytu_dle_delky = {}
-for i in range (0,max_delka+1):
-    pocet_vyskytu=0
-    for polozka in text_k_analyze:
-        delka_slova=len(polozka)
-        if delka_slova==i:
-           pocet_vyskytu=pocet_vyskytu+1
-    pocet_vyskytu_dle_delky[i]=pocet_vyskytu
+# sum of numeric strings
+numb_sum = 0
+for num in numeric_strings:
+    num_int = int(num)
+    numb_sum += num_int
+print ("The sum of all numbers is: ",numb_sum)
 
-#print (type(pocet_vyskytu_dle_delky))
-#print (pocet_vyskytu_dle_delky)
-pocet_hvezdicek=0
-print ("-----------------------------")
-print ("LEN ","|", "OCCURENCES", "|", "NR.", "\n")
-print ("-----------------------------")
-for i in range (1, max_delka+1):
-    pocet_hvezdicek = int(pocet_vyskytu_dle_delky.get (i))
-    print (i,"|", "*" * int(pocet_hvezdicek), "|", pocet_hvezdicek)
-           
-#velka_pismena = 
-#mala_pismena = 
-#cisla_v_textu = 
+# longest word search
+max_word_length=0
+for word in text_to_analyze:
+    word_length = len(word)
+    if word_length > max_word_length:
+        max_word_length = word_length #pocet znaku nejdelsiho slova
+  
+# sum of rate
+rate_by_length  = {}
+for i in range (0,max_word_length+1):
+    length_rate = 0
+    for word in text_to_analyze:
+        word_length = len(word)
+        if word_length == i:
+           length_rate += 1
+    rate_by_length[i]=length_rate
+
+# graphic output
+star_amount = 0
+print ("-" * 34)
+print ("LEN ","|", "OCCURENCES", " " * 10, "|", "NR.")
+print ("-" * 34)
+for i in range (1, max_word_length+1):
+    star_amount = int(rate_by_length.get (i))
+    space_amount = 20 - star_amount
+    space_amount_by_nr = 3 - len(str(i))
+    print (" " * space_amount_by_nr, i,"|", "*" * int(star_amount), " " * space_amount, "|" , star_amount)   
